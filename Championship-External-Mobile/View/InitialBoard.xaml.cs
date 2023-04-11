34c9e7a22ffad0cf91d2b0d41441bb272d261a55
Bruna Lika <brunalika@msn.com>
2023-04-11 18:57:59 -0300

    login page

--- Championship-External-Mobile/View/InitialBoard.xaml.cs
+++ Championship-External-Mobile/View/InitialBoard.xaml.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using ChampionshipExternalMobile.ViewModel;
 using Xamarin.Forms;
 
 namespace ChampionshipExternalMobile.View
 {
     public partial class InitialBoard : ContentPage
     {
+        private InitialBoardViewModel _initialBoardViewModel;
         public InitialBoard()
         {
             InitializeComponent();
+            _initialBoardViewModel = new InitialBoardViewModel();
+            BindingContext = _initialBoardViewModel;
+        }
+
+        void SfButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Console.WriteLine("BOtao pao");
         }
     }
 }
--- Championship-External-Mobile/ViewModel/InitialBoardViewModel.cs
+++ Championship-External-Mobile/ViewModel/InitialBoardViewModel.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using Xamarin.Forms;
+
 namespace ChampionshipExternalMobile.ViewModel
 {
 	public class InitialBoardViewModel : BaseViewModel
     {
         #region Propriedades
+        private string _username;
+        private string _password;
+        private Command _buttonCommand;
         #endregion
 
         public InitialBoardViewModel()
@@ -11,9 +16,27 @@ namespace ChampionshipExternalMobile.ViewModel
 		}
 
         #region Encapsulamento
+        public string Username { get { return _username; } set { _username = value; OnPropertyChanged("Username"); } }
+        public string Password { get { return _password; } set { _password = value; OnPropertyChanged("Password"); } }
         #endregion
 
         #region Commands
+        public Command ButtonCommand => _buttonCommand ?? (_buttonCommand = new Command(async () => {
+
+            try
+            {
+                Console.WriteLine(Username +" "+ Password);
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }));
         #endregion
 
         #region Funções
