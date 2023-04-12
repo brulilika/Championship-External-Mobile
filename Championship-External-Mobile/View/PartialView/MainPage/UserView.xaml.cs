2d6a5929d8a58fe3268d46fc12183ea57d0adebf
Bruna Lika <brunalika@msn.com>
2023-04-12 01:36:54 -0300

    initial user view

--- Championship-External-Mobile/View/PartialView/MainPage/UserView.xaml.cs
+++ Championship-External-Mobile/View/PartialView/MainPage/UserView.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using ChampionshipExternalMobile.ViewModel.PartialViewModel;
 
 namespace ChampionshipExternalMobile.View.PartialView.MainPage
 {	
 	public partial class UserView : ContentView
-	{	
-		public UserView ()
+	{
+		private UserPartialViewModel _userPartialViewModel;
+        public UserView ()
 		{
 			InitializeComponent ();
-		}
+            _userPartialViewModel = new UserPartialViewModel();
+            BindingContext = _userPartialViewModel;
+        }
 	}
 }
 
---
+++ Championship-External-Mobile/ViewModel/PartialViewModel/UserPartialViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Championship_External_Mobile;
+
+namespace ChampionshipExternalMobile.ViewModel.PartialViewModel
+{
+    public class UserPartialViewModel : BaseViewModel
+    {
+        #region Propriedades
+        private List<string> _userFunctions;
+        #endregion
+
+        public UserPartialViewModel()
+        {
+            UserFunctions = new List<string>();
+            LoadData();
+        }
+
+        #region Encapsulamento
+        public List<string> UserFunctions { get { return _userFunctions; } set { _userFunctions = value; OnPropertyChanged("UserFunctions"); } }
+        #endregion
+
+        #region Commands
+        #endregion
+
+        #region Funções
+        public async Task LoadData()
+        {
+            try
+            {
+                UserFunctions.Add("Teste1");
+                UserFunctions.Add("Teste2");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Atenção", "Não foi possível iniciar lista de funções do cliente", "OK");
+            }
+        }
+        #endregion
+    }
+}
+
