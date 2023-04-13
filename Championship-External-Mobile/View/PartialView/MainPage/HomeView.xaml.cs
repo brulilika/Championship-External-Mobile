9541ac8b336f790b144eb858f04d51360d2719b3
Bruna Lika <brunalika@msn.com>
2023-04-12 21:31:21 -0300

    home page view

--- Championship-External-Mobile/View/PartialView/MainPage/HomeView.xaml.cs
+++ Championship-External-Mobile/View/PartialView/MainPage/HomeView.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using ChampionshipExternalMobile.ViewModel.PartialViewModel;
 using Xamarin.Forms;
 
 namespace ChampionshipExternalMobile.View.PartialView.MainPage
 {	
 	public partial class HomeView : ContentView
-	{	
-		public HomeView ()
+	{
+		private HomePartialViewModel _homePartialViewModel;
+        public HomeView ()
 		{
 			InitializeComponent ();
-		}
+			_homePartialViewModel = new HomePartialViewModel();
+			BindingContext = _homePartialViewModel;
+        }
 	}
 }
 
